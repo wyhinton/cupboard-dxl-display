d977325521e02c7db2417920ddf832e271129e0c
Webb Hinton <wyhinton@ncsu.edu>
2021-06-08 11:19:08 -0400

    remove ResizeHandle import

--- src/static/default_layout.ts
+++ src/static/default_layout.ts
@@ -1,18 +1,17 @@
 import { Layout, ResizeHandle } from "react-grid-layout";
-const availableHandles: ResizeHandle[] = [
-  "s",
-  "w",
-  "e",
-  "n",
-  "sw",
-  "nw",
-  "se",
-  "ne",
-];
+// const availableHandles: ResizeHandle[] = [
+//   "s",
+//   "w",
+//   "e",
+//   "n",
+//   "sw",
+//   "nw",
+//   "se",
+//   "ne",
+// ];
 const defaultGridLayout: Layout[] = [
   { i: "clock", x: 0, y: 0, w: 2, h: 1, static: true },
   {
-    resizeHandles: availableHandles,
     i: "0",
     x: 0,
     y: 1,
@@ -21,7 +20,6 @@ const defaultGridLayout: Layout[] = [
     static: true,
   },
   {
-    resizeHandles: availableHandles,
     i: "1",
     x: 1,
     y: 1,
@@ -30,12 +28,12 @@ const defaultGridLayout: Layout[] = [
     minW: 2,
     maxW: 4,
   },
-  { resizeHandles: availableHandles, i: "2", x: 2, y: 1, w: 1, h: 1 },
-  { resizeHandles: availableHandles, i: "3", x: 3, y: 1, w: 1, h: 1 },
-  { resizeHandles: availableHandles, i: "4", x: 0, y: 2, w: 1, h: 1 },
-  { resizeHandles: availableHandles, i: "5", x: 1, y: 2, w: 1, h: 1 },
-  { resizeHandles: availableHandles, i: "6", x: 2, y: 2, w: 1, h: 1 },
-  { resizeHandles: availableHandles, i: "7", x: 3, y: 2, w: 1, h: 1 },
+  { i: "2", x: 2, y: 1, w: 1, h: 1 },
+  { i: "3", x: 3, y: 1, w: 1, h: 1 },
+  { i: "4", x: 0, y: 2, w: 1, h: 1 },
+  { i: "5", x: 1, y: 2, w: 1, h: 1 },
+  { i: "6", x: 2, y: 2, w: 1, h: 1 },
+  { i: "7", x: 3, y: 2, w: 1, h: 1 },
 ];
 
 export default defaultGridLayout;
