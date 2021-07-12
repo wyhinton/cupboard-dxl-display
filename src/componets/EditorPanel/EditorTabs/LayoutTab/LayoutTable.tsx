b67008b6c1c1e7d8dc7e2005b884596d46fa0c3a
Webb Hinton <wyhinton@ncsu.edu>
2021-07-12 12:23:16 -0400

    workflow update

--- src/componets/EditorPanel/EditorTabs/LayoutTab/LayoutTable.tsx
+++ src/componets/EditorPanel/EditorTabs/LayoutTab/LayoutTable.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useStoreState, useStoreActions } from "../../../../hooks";
-
+//test
 const LayoutsTable = () => {
   const externalLayoutsState = useStoreState(
     (state) => state.layoutsModel.externalLayouts
