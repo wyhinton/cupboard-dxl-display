6674e4e45d27414e3f83c18ee713a17edcc82532
Webb Hinton <wyhinton@ncsu.edu>
2021-07-20 23:40:50 -0400

    fix file names

---
+++ src/data_structs/CardData.ts
@@ -0,0 +1,30 @@
+import type RawCardRow from "../interfaces/RawCardRow";
+import { InteractionType } from "../enums";
+/**
+ *
+ */
+export default class CardData {
+  readonly src: string;
+  readonly title: string;
+  readonly added?: Date;
+  readonly sourceId: string;
+  readonly author: string;
+  readonly interaction: InteractionType;
+  isActive!: boolean;
+  // instanceId!: string;
+
+  constructor(row: RawCardRow) {
+    console.log(row);
+    this.src = row.src;
+    this.title = row.title;
+    this.added = new Date(row.added);
+    this.sourceId = row.src;
+    this.author = row.author;
+    this.interaction =
+      InteractionType[row.interaction as keyof typeof InteractionType];
+    this.isActive = false;
+  }
+  set_active(b: boolean) {
+    this.isActive = b;
+  }
+}
