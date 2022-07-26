88343d3c0f4f811f0e7cf8c43ed83b3d3ba5a610
wyhinton <carolinabmx11@gmail.com>
2022-07-26 13:50:53 -0400

    linting fixes

--- src/components/Widgets/HowToUse.tsx
+++ src/components/Widgets/HowToUse.tsx
@@ -3,27 +3,24 @@ import "../../css/clock.css";
 import { HandUpIcon } from "evergreen-ui";
 import React, { useEffect, useState } from "react";
 
-import { AppMode } from "../../enums";
-import { useApp, useToggle } from "../../hooks";
+import { useToggle } from "../../hooks";
 import HowToUsePopUp from "../HowToUse/HowToUsePopUp";
 import Button from "../Shared/Button";
 
 /**
  * Simple clock widget for displaying the current time.
- * @component
  */
 const HowToUse = (): JSX.Element => {
   const [date, setDate] = useState(new Date());
 
   const containerStyle = {
-    height: "100%",
-    // padding: ".25em",
-    width: "100%",
+    backgroundColor: "blue",
+    boxSizing: "border-box",
+    color: "white",
     fontSize: "9vmin",
     fontWeight: 600,
-    color: "white",
-    boxSizing: "border-box",
-    backgroundColor: "blue",
+    height: "100%",
+    width: "100%",
   } as React.CSSProperties;
 
   const [visible, toggleVisible] = useToggle(false);
@@ -39,27 +36,19 @@ const HowToUse = (): JSX.Element => {
     setDate(new Date());
   }
 
-  const { appMode } = useApp();
-
   return (
     <div id="how-to-use-button-widget" style={containerStyle}>
-      {/* {appMode === AppMode.DISPLAY && ( */}
       <HowToUsePopUp active={visible} onClose={toggleVisible} />
-      {/* )} */}
-      {/* {appMode === AppMode.DISPLAY && ( */}
       <Button
         appearance="primary"
         className="how-to-use-button"
         fontSize="xx-large"
-        // height="4vh"
         height="100%"
         iconBefore={<HandUpIcon />}
         onClick={toggleVisible}
-        // width="35vw"
         text="Learn how to use this display"
         width="100%"
       />
-      {/* )} */}
     </div>
   );
 };
